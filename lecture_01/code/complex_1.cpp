e958ae40c09278c48a3d6e742292075cfdcca66d
n1ko1a1 <nikolaimulugin@gmail.com>
2022-08-30 01:30:17 +0500

    first listing

---
+++ lecture_01/code/complex_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream> //headers
+#include<string>
+//g++ -Wall -Werror -g --std=c++20
+//g++ - compiler; -Wall - show all the errors; 
+//-Werror - treat warnings as errors
+int main()//entry point
+{
+    std::string input;//variable with type string
+    std::cin >> input;//standard input
+    int n = input.size();
+    int sum = 0;
+    for( int i = 0; i < n; i++ )//n
+        for( int j = 0; j < 2 * i; j++ )
+        //01 0123 012345 0123456 012..2n-2
+            sum++;//0 + 2 + 4 + 6 + ... 2(n-1)
+    std::cout << sum << std::endl;
+    return 0;//code of return
+}
---
+++ lecture_01/code/test.cpp
@@ -0,0 +1,7 @@
+#include<iostream> //sd
+#include<string>
+//g++ -Wall -Werror -g --std=c++20
+int main()
+{
+    main();
+}
