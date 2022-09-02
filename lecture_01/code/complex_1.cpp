c0579d918dec0efeb9752f4784b5bad35ce9f1c1
n1ko1a1 <nikolaimulugin@gmail.com>
2022-09-02 23:43:59 +0500

    sqrt(n)

--- lecture_01/code/complex_1.cpp
+++ lecture_01/code/complex_1.cpp
@@ -11,7 +11,7 @@ int main()//entry point
     int sum = 0;
     for( int i = 0; i < n; i++ )//n
         for( int j = 0; j < 2 * i; j++ )
-        //01 0123 012345 0123456 012..2n-2
+        //01 0123 012345 0123456 012..2i-1
             sum++;//0 + 2 + 4 + 6 + ... 2(n-1)
     std::cout << sum << std::endl;
     return 0;//code of return
