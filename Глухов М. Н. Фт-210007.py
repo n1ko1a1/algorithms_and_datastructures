f331c744ae991b9e2acdb3208023e7e392723a0b
motik kk <matvey.glukhov@urfu.me>
2022-11-19 13:49:39 +0500

    Solution

---
+++ Глухов М. Н. Фт-210007.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+a = []
+for i in range(n + 1):
+    a.append(i)
+
+a[1] = 0
+
+i = 2
+while i <= n:
+    if a[i] != 0:
+        j = i + i
+        while j <= n:
+            a[j] = 0
+            j = j + i
+    i += 1
+a = set(a)
+a.remove(0)
+print(a)
