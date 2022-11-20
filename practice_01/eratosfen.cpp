fe4a6db468452de6e39e3ea83073e6e833d779a4
Алексей Устинов <102724749+holodnyisiemens@users.noreply.github.com>
2022-11-21 01:07:15 +0500

    ФТ-210007 Устинов

--- practice_01/eratosfen.cpp
+++ practice_01/eratosfen.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
 void PrintBoolArray(bool* arr, int len)
 {
-    for( int i =0; i < len; i++)
+    std::cout << "Простые числа, меньшие или равные " << len << ":\n";
+    for (size_t k = 2; k <= len; k++)
     {
-        std::cout << arr[i] << " ";
+        if (arr[k] == 1)
+        {
+            for (size_t j = k + 1; j <= len; j++)
+            {
+                if (j % k == 0)
+                {
+                    arr[j] = false;
+                }
+            }
+        }
+        else
+        {
+            continue;
+        }
+        std::cout << k << " ";
     }
-    std::cout << std::endl;
+    std::cout << "\nРабота программы завершена" << std::endl;
 }
 
 void FillBoolArray(bool* arr, int len, bool value)
 {
-    for( int i =0; i < len; i++)
+    for (int i = 0; i <= len; i++)
     {
         arr[i] = value;
     }
 }
-//inpit n
-//asdasd
 int main() {
-//0 1 2 3 4 5 6
-//- - + + - 5
-//p = 2 [2] = false
-    int n;
-    std::cin >> n;
+    setlocale(LC_ALL, "Russian");
+
+    int n = 0;
+    bool good = true;
+
+    do {
+        std::cout << "Введите целое число, которое не будут превосходить простые числа: ";
+        std::cin >> n;
+        if (!(good = std::cin.good())) {
+            std::cout << "Ошибка ввода. Попробуйте еще раз\n" << std::endl;
+        }
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } while (!good);
+
     bool* numbers = new bool[n];
 
     FillBoolArray(numbers, n, true);
     PrintBoolArray(numbers, n);
-
 
     return 0;
 }
