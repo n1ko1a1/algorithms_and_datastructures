3f348d14d3782d31ef2d8f7b969d4b4698905209
Shmygol <113779803+saschaschmygol@users.noreply.github.com>
2022-11-21 20:44:29 +0500

    Shmygol_A_P_ft_210007

---
+++ lecture_01/homework_01/Shmygol_A_P_ft_210007.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void Eratos(bool massiv[], int N)                          //*создание функции, получающей на вход массив и число n, до которого нужно найти простые числа*//
+{
+	int x, y;
+	for (y = 2; y <= N; y++) massiv[y] = true;               
+	y = 2;
+	while (y * y <= N)
+	{
+		x = y * y;
+		if (massiv[y])
+			while (x <= N)
+			{
+				massiv[x] = false;
+				x = x + y;
+			}
+		y = y + 1;
+	}
+	cout << "Список простых чисел: ";
+	for (y = 2; y <= N; y++)
+	{
+		if (massiv[y] == true) cout << " " << y;               //вывод простыъ чисел
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");                                    //*надписи на русском*//
+	int N;
+	cout << "введите размер массива" << endl;
+	cin >> N;
+	bool* massiv = new bool[N];
+	Eratos(massiv, N);                                              // применение функции
+	cout << endl;
+}
