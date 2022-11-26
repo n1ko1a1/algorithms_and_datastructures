e09f7747b35234d71c293be5fe734ede47c5b5f6
Алексей Устинов <102724749+holodnyisiemens@users.noreply.github.com>
2022-11-26 12:38:40 +0500

    ФТ-210007 Устинов

--- practice_02/vec_prac.cpp
+++ practice_02/vec_prac.cpp
@@ -2,71 +2,36 @@
 #include <vector>
 #include <iostream>
 #include <climits>
-#include <string>
 
 template<typename T>
-void PrintLenCapacityVal(std::vector<T> & vec)
-{
-    std::cout << "len: " << vec.size() << "; capacity: " << vec.capacity() << std::endl;
-}
-template<typename T>
-void FillVec(std::vector<T> & vec, int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        vec.push_back(i);
-    }
-}
-template<typename T>
-void PrintVec(std::vector<T> & vec)
+T Sort(T& vec)
 {
     int len = vec.size();
-    for(int i = 0; i < len; i++)  //3  0 1 2
-    {
-        std::cout << "value: " << vec[i] << std::endl;
-    }
-}
-template<typename T>
-int FindMax(std::vector<T> & vec)
-{
-    int max = INT_MIN;
-    int len = vec.size();
-    for(int i = 0; i < len; i++)
-    {
-        if (vec[i] > max)
-            max = vec[i];
-    }
-    return max;
-}
 
-template<typename T>
-void PrintLenCapacityValTemplate(T & vec)
-{
-    std::cout << "len: " << vec.size() << "; capacity: " << vec.capacity() << std::endl;
+    for (int i = 0; i < len - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < len; j++)
+        {
+            if (vec[j] < vec[min_index])
+            {
+                min_index = j;
+            }
+        }
+        std::swap(vec[i], vec[min_index]);
+    }
+    return vec;
+    
 }
 
 int main()
 {
-    int i;
-    std::string s;
-    bool b;
-    char c;
+    std::vector<int> vec = {3, 2, 3, 8, 4, 1, 5, 8, 9, 0};
+    std::vector<int> sorted_vec = Sort(vec);
 
-    std::cout << "sizeof int: " << sizeof(i) << std::endl;
-    std::cout << "sizeof string: " << sizeof(s) << std::endl;
-    std::cout << "sizeof bool: " << sizeof(b) << std::endl;
-    std::cout << "sizeof bool: " << sizeof(bool) << std::endl;
-    std::cout << "sizeof char: " << sizeof(c) << std::endl;
-    std::cout << "CHAR_BIT: " << CHAR_BIT << std::endl;
-
-    std::vector<bool> vec_b(1024);
-    std::cout << "sizeof vec_b: " << sizeof(vec_b) << std::endl;
-    std::cout << "size of 1024 bool: " << 1024*sizeof(bool) << std::endl;
-
-
-
-
-
-
+    for (int i = 0; i < sorted_vec.size(); i++)
+    {
+        std::cout << sorted_vec[i] << "; ";
+    }
     return 0;
 }
