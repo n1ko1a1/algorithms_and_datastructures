67bb74963d93eac4032bf110535a3ec2f6086c30
Shmygol <113779803+saschaschmygol@users.noreply.github.com>
2022-12-04 18:06:36 +0500

    Laba2_ShmygolAP

---
+++ practice_02/Laba2_algoritmi.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <random>
+
+using namespace std;
+
+
+//Вывод массивa
+template<typename T>
+void print(const vector<T>& v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+}
+
+
+// coртировка вектора чисел
+template<typename T>
+void Sort_n(vector<T> &v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		for (int k = 0; k < v.size() - i -1; k++)
+		{
+			if (v[k] < v[k+1])
+			{
+				swap(v[k], v[k+1]);
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	vector<int> a(22, 0);                   // создание вектора случайных чисел
+	generate(a.begin(), a.end(), rand);
+
+	Sort_n(a);
+	print(a);
+	return 0;
+}
+
+
+
