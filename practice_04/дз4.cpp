f995968c160fdd0fc854ed895b0b5215245debd3
KatyaLivintseva <113354368+KatyaLivintseva@users.noreply.github.com>
2022-12-05 17:31:41 +0500

    Фт-210008, Ливинцева Катя, дз4

---
+++ practice_04/дз4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() 
+{
+    string stroka; 
+    string bukva;
+    int k;
+    cout<<"Введите d для расшифровки и e для шифровки"<<endl;
+    cin>>bukva;
+    if (bukva=="d")
+    {
+        cout<<"введите строку, которую нужно расшифровать"<<endl;
+        cin>>stroka;
+        cout<<"введите шаг сдвига"<<endl;
+        cin >> k;
+
+        for (auto & alf : stroka) 
+        {
+            if (alf >= 'a' && alf <= 'z') 
+                alf = ((alf - 'a' + k) % 26) + 'a';
+        }
+    }
+    
+    else if (bukva=="e")
+    {
+        cout<<"введите строку, которую нужно зашифровать"<<endl;
+        cin>>stroka;
+        cout<<"введите шаг сдвига"<<endl;
+        cin >> k;
+
+        for (auto & alf : stroka) 
+        {
+            if (alf >= 'a' && alf <= 'z') 
+                alf = ((alf - 'a' - k) % 26) + 'a';
+        }        
+    }
+    cout << stroka << endl;
+}
