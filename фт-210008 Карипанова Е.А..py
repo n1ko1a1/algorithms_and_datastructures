d466cf499648b6dc9b0f9f4de25daafbab671e5d
EKataya <91426628+EKataya@users.noreply.github.com>
2022-12-08 10:42:21 +0500

    добавили файл программы

--- фт-210008 Карипанова Е.А..py
+++ фт-210008 Карипанова Е.А..py
@@ -1,20 +1,28 @@
-n = int(input())
-
-a = []
-for i in range(n + 1):
-    a.append(i)
- 
-a[1] = 0
-
-i = 2
-while i <= n:
-    if a[i] != 0:
-        j = i + i
-        while j <= n:
-            a[j] = 0
-            j = j + i
-    i += 1
- 
-a = set(a)
-a.remove(0)
-print(a)
+#include <iostream>
+using  namespacestd;
+int main()
+{
+    int n;
+    cout << "n=";
+    cin >> n;
+    int *= = * int[n + 1];
+    for (int i = 0; i <= n; i++)
+    a[i] = i;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (a[i])
+        for (int j = i*i; j <= n; j += i)
+        a[j] = 0;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (a[i])
+        {
+            cout << a[i] << ' '; 
+        } 
+    } 
+    
+    cout << endl << endl;
+    delete[] a;
+    0 0;
+}
