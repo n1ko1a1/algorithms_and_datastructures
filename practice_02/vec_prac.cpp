8bd02669f1b677dd8f883baad6ef139bd0ad132a
YWKBM <89938515+YWKBM@users.noreply.github.com>
2022-12-12 14:14:12 +0500

    ФТ-210007 Титов дз2

--- practice_02/vec_prac.cpp
+++ practice_02/vec_prac.cpp
@@ -1,72 +1,35 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <climits>
-#include <string>
-
-template<typename T>
-void PrintLenCapacityVal(std::vector<T> & vec)
-{
-    std::cout << "len: " << vec.size() << "; capacity: " << vec.capacity() << std::endl;
-}
-template<typename T>
-void FillVec(std::vector<T> & vec, int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        vec.push_back(i);
-    }
-}
-template<typename T>
-void PrintVec(std::vector<T> & vec)
-{
-    int len = vec.size();
-    for(int i = 0; i < len; i++)  //3  0 1 2
-    {
-        std::cout << "value: " << vec[i] << std::endl;
-    }
-}
-template<typename T>
-int FindMax(std::vector<T> & vec)
-{
-    int max = INT_MIN;
-    int len = vec.size();
-    for(int i = 0; i < len; i++)
-    {
-        if (vec[i] > max)
-            max = vec[i];
-    }
-    return max;
+void PrintVector(std::vector<int>& vctr) {
+	for (int i = 0; i < vctr.size(); i++) {
+		std::cout << vctr[i] << std::endl;
+	}
 }
 
-template<typename T>
-void PrintLenCapacityValTemplate(T & vec)
-{
-    std::cout << "len: " << vec.size() << "; capacity: " << vec.capacity() << std::endl;
+void QSort(std::vector<int>& vctr, int first, int last) {
+	if (first < last) {
+		int left = first;
+		int right = last;
+		int middle = vctr[(left + right) / 2];
+		do {
+			while (vctr[left] < middle)
+				left++;
+			while (vctr[right] > middle)
+				right--;
+			if (left <= right) {
+				int tmp = vctr[left];
+				vctr[left] = vctr[right];
+				vctr[right] = tmp;
+				left++;
+				right--;
+			}
+		} while (left < right);
+		QSort(vctr, first, right);
+		QSort(vctr, left, last);
+	}
 }
 
 int main()
 {
-    int i;
-    std::string s;
-    bool b;
-    char c;
-
-    std::cout << "sizeof int: " << sizeof(i) << std::endl;
-    std::cout << "sizeof string: " << sizeof(s) << std::endl;
-    std::cout << "sizeof bool: " << sizeof(b) << std::endl;
-    std::cout << "sizeof bool: " << sizeof(bool) << std::endl;
-    std::cout << "sizeof char: " << sizeof(c) << std::endl;
-    std::cout << "CHAR_BIT: " << CHAR_BIT << std::endl;
-
-    std::vector<bool> vec_b(1024);
-    std::cout << "sizeof vec_b: " << sizeof(vec_b) << std::endl;
-    std::cout << "size of 1024 bool: " << 1024*sizeof(bool) << std::endl;
-
-
-
-
-
-
-    return 0;
+	std::vector<int> nums = {50, 3, 1, 2, 12, 5, 6, 4, 0};
+	QSort(nums, 0, nums.size() - 1);
+	PrintVector(nums);
 }
