360dc3210c9c692392f6ce208eec6fa614a039f3
Meylor <114622207+Footsin@users.noreply.github.com>
2022-12-13 21:23:44 +0300

    Update strs.cpp

--- practice_04/strs.cpp
+++ practice_04/strs.cpp
@@ -1,78 +1,40 @@
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <ctime>
-
-bool IsAllowedChar(char c)
+#include <vector>
+using namespace std;
+int main() 
 {
-    if( ( c >= 65 && c <= 90 ) ||/*a-z*/
-        ( c >= 97 && c <= 122) ||/*A-Z*/
-        ( c >= 48 && c <= 57 ) ) /*0-9*/
-        return true;
-    return false;
-}
-
-/*
-bool IsLatinWord(std::string word)
-{
-    for (char c : word )
+    string stroka; 
+    string bukva;
+    int k;
+    cout<<"Введите d для расшифровки и e для шифровки"<<endl;
+    cin>>bukva;
+    if (bukva=="d")
     {
-        if( !IsLatinChar(c) )
-            return false;
-    }
-    return true;
-}*/
+        cout<<"введите строку, которую нужно расшифровать"<<endl;
+        cin>>stroka;
+        cout<<"введите шаг сдвига"<<endl;
+        cin >> k;
 
-
-int main()
-{
-    /*std::string word_1;
-    std::string word_2;
-    std::cin >> word_1 >> word_2;
-
-    std::cout << word_1 + word_2 << std::endl;
-
-    std::cout << word_1 << " " << &word_1 << " " << word_1.length() << std::endl;
-    word_1 += "a";
-    std::cout << word_1 << " " << &word_1 << " " << word_1.length() << std::endl;
-
-    for (char & c : word_1)
-    {
-        char z = 'z';
-        c = z;
-        std::cout << c << std::endl;
-    }
-    std::cout << word_1 << " " << &word_1 << " " << word_1.length() << std::endl;*/
-
-    /*
-    char a = 'a';
-    std::cout << static_cast<int>(a) << std::endl;*/
-    /*
-    std::string word;
-    //std::cin >> word;
-    getline(std::cin, word);
-    std::cout << IsLatinWord(word) << std::endl;*/
-
-    int len;
-    std::cin >> len;
-    std::string allowed_char = "a-Z,0-9";
-    char c = 97;
-    //std::cout << ('b' < 'z') << std::endl;
-    //48 - 122
-    std::srand(time(0));
-    std::string password;
-    int cnt = 0;
-    while (cnt < len)
-    {
-        char candidate = 48 + rand()%74;
-        if (IsAllowedChar(candidate))
+        for (auto & alf : stroka) 
         {
-            password +=candidate;
-            cnt++;
+            if (alf >= 'a' && alf <= 'z') 
+                alf = ((alf - 'a' + k) % 26) + 'a';
         }
     }
-    std::cout << password << std::endl;
 
+    else if (bukva=="e")
+    {
+        cout<<"введите строку, которую нужно зашифровать"<<endl;
+        cin>>stroka;
+        cout<<"введите шаг сдвига"<<endl;
+        cin >> k;
 
-    return 0;
+        for (auto & alf : stroka) 
+        {
+            if (alf >= 'a' && alf <= 'z') 
+                alf = ((alf - 'a' - k) % 26) + 'a';
+        }        
+    }
+    cout << stroka << endl;
 }
