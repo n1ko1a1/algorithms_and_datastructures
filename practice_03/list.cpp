4dab58825f329b398f137ebcd038ad77814a11cc
Meylor <114622207+Footsin@users.noreply.github.com>
2022-12-13 21:16:50 +0300

    Update list.cpp

--- practice_03/list.cpp
+++ practice_03/list.cpp
@@ -1,59 +1,68 @@
 #include <iostream>
 #include <list>
-#include <set>
 
-bool predicat(int val)
+static bool check_sequence(std::string brackets, std::list<char> brackets_list)
 {
-    return val < 2;
+	int count_left = 0;
+	int count_right = 0;
+	std::list<char>::iterator iter = brackets_list.begin();
+
+	while (iter != brackets_list.end())
+	{
+		if (*iter == brackets[0])
+		{
+			count_left += 1;
+		}
+		else if (*iter == brackets[1])
+		{
+			if (count_left > count_right)
+			{
+				count_right += 1;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		++iter;
+	}
+	return count_left == count_right;
 }
 
 int main()
 {
-    /*std::list<int> v = {1,2,3,4};
-    std::list<int>::iterator iter = v.end();
-
-    while(iter != v.begin())
-    {
-        --iter;
-        std::cout << *iter << std::endl;
-
-    }*/
-    /*
-    for (int e : v)
-    {
-        std::cout << e << std::endl;
-    }*/
-    //std::list<int> lst;
-    int input_val=0;
-    /*while(true)
-    {
-        std::cin >> input_val;
-        if (input_val == 0)
-            break;
-        lst.push_back(input_val);
-    }*/
-    /*lst.sort();
-    lst.unique();*/
-    std::set<int> s;
-    /*for (int e : lst)
-    {
-        if (!s.contains(e))
-        {
-            s.insert(e);
-            std::cout << e << std::endl;
-        }
-    }*/
-
-    int line = 3;
-    std::list<int> lst = {1,2,3,4,5};
-    std::size_t res = lst.remove_if( [line](int val){return val < line;} );
-    //std::size_t res = lst.remove_if(predicat);
-    std::cout << "res: " << res << std::endl;
-    for (int e : lst)
-    {
-        std::cout << e << std::endl;
-    }
-
-
-    return 0;
+	char input_value;
+	std::list<char> round_brackets;
+	std::list<char> square_brackets;
+	std::list<char> curly_brackets;
+	std::cout << "You can enter '(', ')', '[', ']', '{' or '}' until you enter 0." << std::endl
+		<< "true - all brackets are closed else false" << std::endl;;
+	while (true)
+	{
+		std::cin >> input_value;
+		if (input_value == '0')
+		{
+			break;
+		}
+		else if (input_value == '(' || input_value == ')')
+		{
+			round_brackets.push_back(input_value);
+		}
+		else if (input_value == '[' || input_value == ']')
+		{
+			square_brackets.push_back(input_value);
+		}
+		else if (input_value == '{' || input_value == '}')
+		{
+			curly_brackets.push_back(input_value);
+		}
+		else
+		{
+			std::cout << "Input Error" << std::endl;
+		}
+	}
+	bool result = check_sequence("()", round_brackets) && check_sequence("[]", square_brackets) && check_sequence("{}", curly_brackets);
+	std::cout.setf(std::ios_base::boolalpha);
+	std::cout << result;
+	return 0;
 }
