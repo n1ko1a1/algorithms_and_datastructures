4192840adb53edf986da23f3bcf1a9a86bb775e6
Потапов Даниил <113824271+potopik@users.noreply.github.com>
2022-12-18 04:01:52 +0500

    homework2 Потапов

--- practice_02/vec_prac.cpp
+++ practice_02/vec_prac.cpp
@@ -1,72 +1,29 @@
-#include <string>
-#include <vector>
 #include <iostream>
-#include <climits>
-#include <string>
-
-template<typename T>
-void PrintLenCapacityVal(std::vector<T> & vec)
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool comparisonFunction(int a, int b)
 {
-    std::cout << "len: " << vec.size() << "; capacity: " << vec.capacity() << std::endl;
-}
-template<typename T>
-void FillVec(std::vector<T> & vec, int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        vec.push_back(i);
-    }
-}
-template<typename T>
-void PrintVec(std::vector<T> & vec)
-{
-    int len = vec.size();
-    for(int i = 0; i < len; i++)  //3  0 1 2
-    {
-        std::cout << "value: " << vec[i] << std::endl;
-    }
-}
-template<typename T>
-int FindMax(std::vector<T> & vec)
-{
-    int max = INT_MIN;
-    int len = vec.size();
-    for(int i = 0; i < len; i++)
-    {
-        if (vec[i] > max)
-            max = vec[i];
-    }
-    return max;
-}
-
-template<typename T>
-void PrintLenCapacityValTemplate(T & vec)
-{
-    std::cout << "len: " << vec.size() << "; capacity: " << vec.capacity() << std::endl;
+    return a < b;
 }
 
 int main()
 {
-    int i;
-    std::string s;
-    bool b;
-    char c;
+	setlocale(LC_ALL, "Russian");
+    vector<int> vec = {1, 9, 6, 2, 0, 4, 10, 18, 5};
+    vector<int>::iterator index;
+    cout << "Вектор до сортировки: ";
+    for (index = vec.begin(); index != vec.end(); index++)
+    {
+        cout << *index << " ";
+    }
+    sort(vec.begin(), vec.end(), comparisonFunction);
 
-    std::cout << "sizeof int: " << sizeof(i) << std::endl;
-    std::cout << "sizeof string: " << sizeof(s) << std::endl;
-    std::cout << "sizeof bool: " << sizeof(b) << std::endl;
-    std::cout << "sizeof bool: " << sizeof(bool) << std::endl;
-    std::cout << "sizeof char: " << sizeof(c) << std::endl;
-    std::cout << "CHAR_BIT: " << CHAR_BIT << std::endl;
-
-    std::vector<bool> vec_b(1024);
-    std::cout << "sizeof vec_b: " << sizeof(vec_b) << std::endl;
-    std::cout << "size of 1024 bool: " << 1024*sizeof(bool) << std::endl;
-
-
-
-
-
+    cout << "\nОтсортированный вектор: ";
+    for (index = vec.begin(); index != vec.end(); index++)
+    {
+        cout << *index << " ";
+    }
 
     return 0;
 }
