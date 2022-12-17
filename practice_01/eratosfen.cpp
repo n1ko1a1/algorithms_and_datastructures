24d2e1179362c5dd8b45b753c43fcffee6d28d2a
Потапов Даниил <113824271+potopik@users.noreply.github.com>
2022-12-18 01:51:16 +0500

    Update eratosfen.cpp
    homework1

--- practice_01/eratosfen.cpp
+++ practice_01/eratosfen.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <limits> 
 using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian"); //подключение русского языка
-    cout << "\nВведите целочисленное количоство чисел N : ";
+    cout << "\nВведите целочисленное количество чисел N : ";
     int n;
     std::cin >> n;
     if (cin.fail()) {
@@ -16,7 +17,7 @@ int main() {
     }
     int* p = new int[n]; //Освобождаем память для динамического массива
     for (int i = 0; i < n; i++) // Создаем массив размерностью N 
-        p[i] = { 0 };
+        p[i] = 0;
     for (int i = 2; i < n; i++) { // Цикл замены 0 на 1 каждого непростого числа в массиве
         for (int j = i * i; j < n + 1; j += i) {
             p[j - 1] = 1;
@@ -28,7 +29,6 @@ int main() {
         if (p[i - 1] == 0)
             std::cout << i << "\t";
     }
-
     return 0;
 }
 //ФТ-210007_Потапов_Даниил_Практика_1
