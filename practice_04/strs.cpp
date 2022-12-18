4a759498918da49c619202283ef4229c01f074cf
Потапов Даниил <113824271+potopik@users.noreply.github.com>
2022-12-18 05:00:59 +0500

    Update strs.cpp

--- practice_04/strs.cpp
+++ practice_04/strs.cpp
@@ -1,78 +1,39 @@
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <ctime>
-
-bool IsAllowedChar(char c)
+using namespace std;
+ 
+int checkPar(const string& instr, int pos = 0)
 {
-    if( ( c >= 65 && c <= 90 ) ||/*a-z*/
-        ( c >= 97 && c <= 122) ||/*A-Z*/
-        ( c >= 48 && c <= 57 ) ) /*0-9*/
-        return true;
-    return false;
-}
-
-/*
-bool IsLatinWord(std::string word)
-{
-    for (char c : word )
+    static const int len = instr.length();
+    static const string light = "([{";
+    static const string right = ")]}";
+    while (pos < len)
     {
-        if( !IsLatinChar(c) )
-            return false;
-    }
-    return true;
-}*/
-
-
+        const size_t tokpos = light.find_first_of(instr[pos]);
+        if (tokpos != string::npos)
+        {
+            pos = checkPar(instr, pos + 1);
+            if (pos < 0 || pos >= len || instr[pos] != right[tokpos]) return -1;
+        }
+        else
+            if (right.find_first_of(instr[pos]) != string::npos) return pos;       
+        ++pos;
+    }   
+    return pos;
+}
+ 
 int main()
 {
-    /*std::string word_1;
-    std::string word_2;
-    std::cin >> word_1 >> word_2;
-
-    std::cout << word_1 + word_2 << std::endl;
-
-    std::cout << word_1 << " " << &word_1 << " " << word_1.length() << std::endl;
-    word_1 += "a";
-    std::cout << word_1 << " " << &word_1 << " " << word_1.length() << std::endl;
-
-    for (char & c : word_1)
+	cout << "Enter a text: ";
+    string scobki;    
+    getline(cin, scobki);
+    if (checkPar(scobki) == scobki.length())
     {
-        char z = 'z';
-        c = z;
-        std::cout << c << std::endl;
+        cout << "True";
     }
-    std::cout << word_1 << " " << &word_1 << " " << word_1.length() << std::endl;*/
-
-    /*
-    char a = 'a';
-    std::cout << static_cast<int>(a) << std::endl;*/
-    /*
-    std::string word;
-    //std::cin >> word;
-    getline(std::cin, word);
-    std::cout << IsLatinWord(word) << std::endl;*/
-
-    int len;
-    std::cin >> len;
-    std::string allowed_char = "a-Z,0-9";
-    char c = 97;
-    //std::cout << ('b' < 'z') << std::endl;
-    //48 - 122
-    std::srand(time(0));
-    std::string password;
-    int cnt = 0;
-    while (cnt < len)
+    else
     {
-        char candidate = 48 + rand()%74;
-        if (IsAllowedChar(candidate))
-        {
-            password +=candidate;
-            cnt++;
-        }
-    }
-    std::cout << password << std::endl;
-
-
+        cout << "False";
+    }   
     return 0;
 }
