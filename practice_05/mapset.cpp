1f4825fbae6614d8974b720db429d4a0d9806e95
YWKBM <89938515+YWKBM@users.noreply.github.com>
2022-12-23 01:28:43 +0500

    Титов Фт-210007 Дз 5

--- practice_05/mapset.cpp
+++ practice_05/mapset.cpp
@@ -1,97 +1,61 @@
 #include <iostream>
-#include <set>
 #include <map>
 #include <string>
+#include <vector>
 
-//n - комманд
-//1 - добавить элемент в мн-во
-//2 - проверить есть ли элемент в мн-ве
-//3 - удалить эл-т
-
-template< typename T>
-class MySet
-{
-public:
-    void AddElement(T el){}
-private:
-    std::set<T> data_;
-};
-/*
-int main( int argc, char ** arcgv )
-{
-    std::set<int> s;
-    int type, n, value;
-    std::cin >> n;
-    std::cout << "argc: " << argc << std::endl;
-    for(int i =0; i < argc; i++)
-    {
-        std::cout<< arcgv[i] << std::endl;
-    }
-
-
-    for(int i = 0; i < n; i++)
-    {
-        std::cin >>type;
-        std::cin >> value;
-
-        if (type == 1)
-        {
-            s.insert(value);
-        }
-        else if (type==2)
-        {
-            auto found = s.find(value);
-            if (found != s.end() )
-                std::cout << value << " found in the set\n";
-            else
-                std::cout << value << " is not found in the set\n";
-        }
-        else
-        {}
-    }
-
-    return 0;
-}
-*/
-/*
-int main( int argc, char ** arcgv )
-{
-    std::set<int> s{1,2,3, 4, 5}; // 1 2 3 < 4 5 6
-    for ( auto current = s.begin(); current != s.end(); current++)
-    {
-        std::cout << *current << std::endl;
-    }
-    auto bound = s.upper_bound(4);
-    std::cout << "bound " << *bound << std::endl;
-
-}*/
-
-std::ostream& operator<<(std::ostream& os, std::pair<std::string, int> p)
-{
-    os << "key: " << p.first << "; value: " << p.second << std::endl;
-    return os;
+std::vector<std::string> Split(std::string str) {
+	std::string word = "";
+	std::vector<std::string> res;
+	int i = 0;
+	while (i <= str.length()) {
+		if (str[i] <= ' ') {
+			res.push_back(word);
+			word = "";
+		}
+		else {
+			word += str[i];
+		}
+		i++;
+	}
+	return res;
 }
 
-int main( int argc, char ** arcgv )
-{
-    std::map<std::string, int> my_map;//pair<>
-    my_map.insert({"Kozume", 8355});
-    my_map.insert({"Ab", 8235});
-
-    std::map<std::string, std::vector<int>> my_map_2;//pair<>
-    my_map.insert({"Kozume", 8355});
-
-    for ( auto current = my_map.begin(); current != my_map.end(); current++)
-    {
-        std::cout << *current << std::endl;
-    }
-
-    for ( auto pa : my_map)
-    {
-        std::cout << pa.first << " " << pa.second << std::endl;
-    }
-
+std::map<std::string, int> MappedWords(std::vector<std::string> words) {
+	std::map<std::string, int> mp;
+	auto iter = words.begin();
+	while (iter != words.end())    
+	{
+		std::map<std::string, int>::iterator it = mp.find(*iter);
+		if (it == mp.end()) {
+			mp.emplace(*iter, 0);
+		}
+		else {
+			it->second++;
+		}
+		++iter;
+	}
+	return mp;
 }
-//asdjkfh sjdfhalk sjkahfl
+
+std::string MostMet(std::map<std::string, int> mp) {
+	std::map<std::string, int>::iterator iter = mp.begin();
+	auto max = mp.begin();
+	while (iter != mp.end()) {
+		if (iter->second > max->second) {
+			max = iter;
+		}
+		++iter;
+	}
+	std::string res = max->first;
+	return res;
+}
+
+int main()
+{
+	std::cout << "Enter the text to find the most frequent word" << std::endl;
+	std::string text;
+	std::cin >> text;
+	std::cout << MostMet(MappedWords(Split(text)));
+}
 
 
