ad1e1de0a776fd5c8e255d136dab7d091fa21e37
Потапов Даниил <113824271+potopik@users.noreply.github.com>
2022-12-22 23:38:38 +0500

    create lab5

---
+++ practice_05/lab5.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    const int N = 256;
+    map <int, string> mp;
+
+
+
+    string S;
+    cout << "Введите строчку на английском";
+    getline(cin, S);
+
+
+    char word[N] = {};
+    stringstream x;
+    x << S; 
+    int i = 1;           
+    while (x >> word)
+    {
+
+        mp[i] = word;
+        i += 1;
+    }
+
+    map <int, string> ::iterator it = mp.begin();
+    map <int, string> ::iterator it2 = mp.begin();
+
+    int itog1[256] = {};
+    string itog2[256] = {};
+
+    for (int i = 0; it != mp.end(); it++, i++)
+    {
+        itog2[i] = it->first;
+        for (int x = 0; it2 != mp.end(); it2++, x++)
+        {
+
+            if (it->second == it2->second)
+            {
+                itog2[i] = it2->second;
+                itog1[i] += 1;
+            }
+        }
+        it2 = mp.begin();
+    }
+
+    int max = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        if (itog1[i] > max)
+        {
+            max = itog1[i];
+        }
+    }
+
+    int ii = 0;
+    int index = 0;
+
+    for (int b : itog1)
+    {
+        if (b == max)
+        {
+            index = ii;
+            break;
+        }
+        ii += 1;
+    }
+
+    cout << "Слово  " << itog2[index] << " " << max;
+    cin.get();
+    return 0;
+} 
