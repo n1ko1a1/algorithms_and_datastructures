a80d19e541a4c4f00ce1ad6433900bc434bfb449
motik kk <matvey.glukhov@urfu.me>
2022-12-24 12:24:14 +0500

    Глухов М. Н. Фт-210007 Lab_1

---
+++ Lab_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+void Eratosthenes(bool massiv[], int N)
+{
+	int i, j;
+	for (j = 2; j <= N; j++) massiv[j] = true;
+	j = 2;
+	while (j * j <= N)
+	{
+		i = j * j;
+		if (massiv[j])
+			while (i <= N)
+			{
+				massiv[i] = false;
+				i = i + j;
+			}
+		j = j + 1;
+	}
+	cout << "Список простых чисел: ";
+	for (j = 2; j <= N; j++)
+	{
+		if (massiv[j] == true) cout << " " << j;
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int N;
+	cout << "До какого числа хотите найти простые числа?" << endl;
+	cin >> N;
+	bool* massiv = new bool[N];
+	Eratosthenes(massiv, N);
+	cout << endl;
+	system("pause");
+}
