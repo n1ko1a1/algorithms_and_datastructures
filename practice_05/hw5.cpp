4b109f0f45e53bea3ab8f954892369240b8395e1
maks141 <books141@mail.ru>
2022-12-27 15:00:35 +0500

    Add hw5

---
+++ practice_05/hw5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+
+
+int main(int argv, char ** arcgv)
+{
+    char c;
+    bool in_word = false;
+    std::string word;
+    std::pair<std::string, int> freq;
+    std::map<std::string, int> counts_words;
+
+    while (1)
+    {   
+        c = getchar();
+
+        if (c == EOF) 
+            break;
+
+        if ((isspace(c) || ispunct(c)) && in_word == true)
+        {
+            ++counts_words[word];
+            in_word = false;
+            word.clear();
+        }
+        else
+        {
+            in_word = true;
+            word.push_back(c);
+        }
+    }
+    if (in_word)
+        ++counts_words[word];
+
+    for (auto cw : counts_words)
+        if (cw.second > freq.second)
+            freq = cw;
+
+    if (freq.second)
+        std::cout << std::endl << freq.first << " " << freq.second << std::endl;
+    else
+        std::cout << std::endl << "String is empty." << std::endl;
+}
