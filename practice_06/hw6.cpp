15781e12d0368dcd67cc36b27a0d481d76a46d8b
EKataya <91426628+EKataya@users.noreply.github.com>
2023-01-04 15:02:21 +0500

    Add files via upload

---
+++ practice_06/hw6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <climits> 
+
+struct Node {
+    Node* l;
+    Node* r;
+    int data;
+};
+
+bool SearchTree(Node* node, int minValue, int maxValue) {
+    if (node == nullptr) {
+        return true;
+    }
+
+    if (node->data < minValue || node->data > maxValue) {
+        return false;
+    }
+
+    return SearchTree(node->l, minValue, node->data - 2) &&
+        SearchTree(node->r, node->data + 2, maxValue);
+}
+
+int main() {
+    Node* root = new Node{ nullptr, nullptr, 4 };
+    root->l = new Node{ nullptr, nullptr, 2 };
+    root->r = new Node{ nullptr, nullptr, 6 };
+
+    bool searchTree = SearchTree(root, INT_MIN, INT_MAX);
+    std::cout << (searchTree ? "true" : "false") << std::endl;
+
+    return 0;
+}
