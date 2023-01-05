f4f81dd6e76100f9a8e59e19e9a97e378b9244e3
motik kk <matvey.glukhov@urfu.me>
2023-01-05 23:10:29 +0500

    Фт-210007 Глухов М. Н. Lab_6

---
+++ practice_06/Lab_6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <climits> 
+
+struct Node {
+    Node* left;
+    Node* right;
+    int data;
+};
+
+bool SearchTree(Node* node, int minValue, int maxValue) {
+    if (node == nullptr) {
+        return true;
+    }
+
+    if (node->data < minValue || node->data > maxValue) {
+        return false;
+    }
+
+    return SearchTree(node->left, minValue, node->data - 2) &&
+        SearchTree(node->right, node->data + 2, maxValue);
+}
+
+int main() {
+    Node* root = new Node{ nullptr, nullptr, 4 };
+    root->left = new Node{ nullptr, nullptr, 2 };
+    root->right = new Node{ nullptr, nullptr, 6 };
+
+    bool searchTree = SearchTree(root, INT_MIN, INT_MAX);
+    std::cout << (searchTree ? "true" : "false") << std::endl;
+
+    return 0;
+}
