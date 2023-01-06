c76a012b3c861ef4acb63186db163861bb78f251
YWKBM <ywkbm@LAPTOP-J5FRTM1M>
2023-01-06 12:32:01 +0300

    File hw3 added

---
+++ practice_03/hw3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+
+bool Check(std::string s) {
+	std::stack<char> openBrackets;
+	for (int i = 0; i < s.length(); i++) {
+		switch (s[i]) {
+		case '(':
+		case '[':
+		case '{':
+			openBrackets.push(s[i]);
+			break;
+		case ')':
+			if (openBrackets.empty()) return false;
+			if (openBrackets.top() != '(') {
+				return false;
+			}
+			else {
+				openBrackets.pop();
+			}
+			break;
+		case ']':
+			if (openBrackets.empty()) return false;
+			if (openBrackets.top() != '[') {
+				return false;
+			}
+			else {
+				openBrackets.pop();
+			}
+			break;
+		case '}':
+			if (openBrackets.empty()) return false;
+			if (openBrackets.top() != '{') {
+				return false;
+			}
+			else {
+				openBrackets.pop();
+			}
+			break;
+		}
+	}
+	return openBrackets.empty();
+}
+
+int main()
+{
+	std::string brackets = "[([({})])]";
+	std::cout << Check(brackets);
+}
