a0c61950a96f001498d5e560c52aaa2c05fb6041
Johannes Mitrofanov <113423156+dr3am1ngg0v@users.noreply.github.com>
2023-01-10 00:03:11 +0500

    Add files via upload
    adding homework4

---
+++ practice_04/lab4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string encrypt(string text, int key) {
+    string result = "";
+
+    for (int i = 0; i < text.length(); i++) {
+        if (isalpha(text[i])) {
+            if (isupper(text[i])) {
+                result += char((int(text[i] + key - 65) % 26) + 65);
+            }
+            else {
+                result += char((int(text[i] + key - 97) % 26) + 97);
+            }
+        }
+        else {
+            result += text[i];
+        }
+    }
+
+    return result;
+}
+
+string decrypt(string text, int key) {
+    return encrypt(text, 26 - key);
+}
+
+int main() {
+    string text;
+    int key;
+
+    cout << "Enter text: ";
+    getline(cin, text);
+
+    cout << "Enter key: ";
+    cin >> key;
+
+    cout << "Original text: " << text << endl;
+    cout << "Encrypted text: " << encrypt(text, key) << endl;
+    cout << "Decrypted text: " << decrypt(text, key) << endl;
+
+    return 0;
+}
